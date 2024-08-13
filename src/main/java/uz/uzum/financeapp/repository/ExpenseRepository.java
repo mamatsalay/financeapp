ed466d4bedfb3d47d8ee7165a982d031fb9df15e
mamatsalay <95539926+mamatsalay@users.noreply.github.com>
2024-08-13 09:17:04 -0400

    rep fix

--- src/main/java/uz/uzum/financeapp/repository/ExpenseRepository.java
+++ src/main/java/uz/uzum/financeapp/repository/ExpenseRepository.java
@@ -3,7 +3,7 @@ package uz.uzum.financeapp.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import uz.uzum.financeapp.model.Expense;
 
-
+@Repository
 public interface ExpenseRepository extends JpaRepository<Expense, Long> {
 
 }
--- src/main/java/uz/uzum/financeapp/repository/IncomeRepository.java
+++ src/main/java/uz/uzum/financeapp/repository/IncomeRepository.java
@@ -3,6 +3,7 @@ package uz.uzum.financeapp.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import uz.uzum.financeapp.model.Income;
 
+@Repository
 public interface IncomeRepository extends JpaRepository<Income, Long> {
 
 }
